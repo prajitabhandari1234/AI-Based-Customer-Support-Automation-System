9211db1c99709767a92601dcea1305d8759f547f
Prajita Bhandari <prajita.bhandari@cqumail.com>
2026-08-09 19:39:09 +1000

    Initialize project folder structure

---
+++ backend/src/main/java/com/cqu/coit13230/AIBasedCustomerSupport/AiBasedCustomerSupportApplication.java
@@ -0,0 +1,13 @@
+package com.cqu.coit13230.AIBasedCustomerSupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBasedCustomerSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBasedCustomerSupportApplication.class, args);
+	}
+
+}
