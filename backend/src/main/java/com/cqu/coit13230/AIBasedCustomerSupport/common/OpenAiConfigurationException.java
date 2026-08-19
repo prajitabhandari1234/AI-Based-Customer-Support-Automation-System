f5a472e211bd6fe32f4c06f1a4fe568ac73040c2
aayushbh23 <aayushbh23@gmail.com>
2026-08-19 19:22:34 +1000

    OpenAI configuration errors added

---
+++ backend/src/main/java/com/cqu/coit13230/AIBasedCustomerSupport/common/OpenAiConfigurationException.java
@@ -0,0 +1,7 @@
+package com.cqu.coit13230.AIBasedCustomerSupport.common;
+
+public class OpenAiConfigurationException extends RuntimeException {
+    public OpenAiConfigurationException(String message) {
+        super(message);
+    }
+}
